2f1c768bc129f7a555ef0be07012f018fb552726
gunubin <kato@condesire.com>
2018-01-23 19:56:39 +0900

    不要なファイルを削除

--- src/config/index.ts
+++
@@ -1,3 +0,0 @@
-export const WP_API_ENDPOINT = process.env.WP_API_ENDPOINT || ''
-export const ACF_API_ENDPOINT = process.env.ACF_API_ENDPOINT || ''
-export const CONTACT_ENDPOINT = process.env.CONTACT_ENDPOINT || ''
--- src/types/index.d.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-declare module "gsap" {
-  export interface TweenConfig {
-    [p: string]: any;
-  }
-}
